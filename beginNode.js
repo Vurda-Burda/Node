0d0f92bf436595b312f7df33f4e48dc9dc60fada
Aleksandr Bolokhov <bolohov@inbox.ru>
2021-02-23 11:04:51 +0300

    homework-3

--- beginNode.js
+++
@@ -1,4 +0,0 @@
-let a = Math.random()*10;
-let b = Math.random()*10;
-const result = Math.pow(a,b);
-console.log(result);
--- homeworkAPI.js
+++
@@ -1,3 +0,0 @@
-
-const homeworkAPI = require ("./stairs");
-console.log(homeworkAPI);
